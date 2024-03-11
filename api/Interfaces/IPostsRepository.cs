20c17a5f42b631e096861cd2f5a5214af63f2a7c
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-11 18:30:05 +0100

    Create posts repository interface

---
+++ api/Interfaces/IPostsRepository.cs
@@ -0,0 +1,14 @@
+using api.Dtos.Post;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IPostsRepository
+    {
+        Task<List<Post>> GetAllAsync();
+        Task<Post?> GetByIdAsync(int id);
+        Task<Post> CreateAsync(Post postModel);
+        Task<Post?> UpdateAsync(int id, UpdatePostDto postDto);
+        Task<Post?> DeleteAsync(int id);
+    }
+}
