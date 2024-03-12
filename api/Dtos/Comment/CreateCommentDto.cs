27d08426ba94e3efcedfbee89aa1acb10583cd1e
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-12 12:59:45 +0100

    Create update and create comment dtos

---
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -0,0 +1,7 @@
+namespace api.Dtos.Comment
+{
+    public class CreateCommentDto
+    {
+        public string Content { get; set; } = string.Empty;
+    }
+}
---
+++ api/Dtos/Comment/UpdateCommentDto.cs
@@ -0,0 +1,7 @@
+namespace api.Dtos.Comment
+{
+    public class UpdateCommentDto
+    {
+        public string Content { get; set; } = string.Empty;
+    }
+}
