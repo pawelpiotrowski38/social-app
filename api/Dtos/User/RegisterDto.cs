ff38cf592678fb62f74505697d6390a08ebd6a21
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-20 17:40:08 +0100

    Create register dto

---
+++ api/Dtos/User/RegisterDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.User
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
